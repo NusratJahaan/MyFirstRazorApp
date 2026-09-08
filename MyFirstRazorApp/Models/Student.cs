6c9c0f8d62ac8092cc7f86e8342b6c50b0132f52
Shanto <zanibul.shanto@gmail.com>
2026-09-08 07:34:20 +0600

    SETP: 1

--- MyFirstRazorApp/Models/Student.cs
+++ MyFirstRazorApp/Models/Student.cs
@@ -1,11 +1,17 @@
 ﻿namespace MyFirstRazorApp.Models
 {
+    //BaseClass
+    // CreatedDate
+    //CreatedBy
+    //UpdatedDate
+    //UpdatedBy
+
     public class Student
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public int Age { get; set; }
-        public string Course { get; set; }
+        public int Id { get; set; } //Primary Key
+        public string Name { get; set; } //Required hote hbe
+        public string Email { get; set; } //Valid Email hote hbe [Regex]
+        public int? Age { get; set; } //nullable
+        public string Course { get; set; } //Enum :: Bangla, English, Math, Science
     }
 }
