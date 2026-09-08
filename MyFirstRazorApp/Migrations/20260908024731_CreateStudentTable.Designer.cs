8e5230aa78b5ce1917f6e0f9cbd1acb253c03b51
NusratJahaan <nusrat.jahaan.dev@gmail.com>
2026-09-07 20:22:18 -0700

    Fix: Updated connection string to SQL Server default instance, added SQLite database files to .gitignore, and created new migrations

--- MyFirstRazorApp/Migrations/20260908024731_CreateStudentTable.Designer.cs
+++ MyFirstRazorApp/Migrations/20260908024731_CreateStudentTable.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyFirstRazorApp.Data;
 
@@ -10,9 +11,11 @@ using MyFirstRazorApp.Data;
 namespace MyFirstRazorApp.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260908024731_CreateStudentTable")]
+    partial class CreateStudentTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
